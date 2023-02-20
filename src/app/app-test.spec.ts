4a2d6ef8d1b294691aa99f34506c52039a1bdb5b
DevTeam237_4 <noubissiepavel@gmail.com>
2023-02-20 14:02:28 +0100

    app.spect.ts

---
+++ src/app/app-test.spec.ts
@@ -0,0 +1,13 @@
+describe('First Test', () => {
+  let testVaribale : any;
+
+  beforeEach(() => {
+    testVaribale = {};
+  });
+
+  it('should return true if a is true'), () =>{
+    testVaribale.a = false;
+    testVaribale=
+
+  }
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularUnitTesting';
+  title = 'Angular Unit Testing';
 }
