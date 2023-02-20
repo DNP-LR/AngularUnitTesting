a7cba10746acaeebaf4f5167dec2701334654d9c
Donfack Noubissie Pavel Le Roi <noubissiepavel@gmail.com>
2023-02-20 14:42:15 +0100

    spec refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularUnitTesting'`, () => {
+  it(`should have as title 'Angular Unit Testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularUnitTesting');
+    expect(app.title).toEqual('Angular Unit Testing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularUnitTesting app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular Unit Testing app is running!');
   });
 });
