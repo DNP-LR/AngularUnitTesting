3f9e60d15efdfdd1f42a07c845532f562c103696
DevTeam237_4 <noubissiepavel@gmail.com>
2023-02-20 19:15:56 +0100

    testing pipe

--- src/app/calculator.service.spec.ts
+++ src/app/calculator.service.spec.ts
@@ -4,23 +4,31 @@ import {CalculatorService} from './calculator.service';
 
 describe('CalculatorService', () => {
   let service: CalculatorService;
-
+  let mockLoggerService : any;
   beforeEach(() => {
+    console.log('Calling before each')
     TestBed.configureTestingModule({});
     service = TestBed.inject(CalculatorService);
+    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    service = new CalculatorService(mockLoggerService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+      expect(service).toBeTruthy();
   });
 
   it('should add two numbers', () =>{
+    console.log('Calling add')
     let result = service.add(2,2);
     expect(result).toBe(4);
-  });
-  it('should subtract two numbers', () =>{
-    let result = service.subtract(2, 2);
-    expect(result).toBe(0)
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
   });
 
+  it('should subtract two numbers', () =>{
+    console.log('Calling subtract')
+
+    let result = service.subtract(2, 2);
+    expect(result).toBe(0)
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
+  });
 });
